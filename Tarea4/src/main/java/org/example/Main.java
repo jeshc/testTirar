4d21c919f27aae94095904ea7af15c355248d3e3
jeshc <hcjesus@gmail.com>
2023-02-20 15:23:51 -0600

    Tarea 4

---
+++ Tarea4/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        for (int i = 10; i >=  0 ; i--) {
+            System.out.println(i);
+        }
+    }
+}
